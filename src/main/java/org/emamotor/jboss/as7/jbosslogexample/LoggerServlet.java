14bfb78e33ee8ecab41f8b03174592e616888c1f
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2013-01-02 11:30:07 +0900

    format comment

--- src/main/java/org/emamotor/jboss/as7/jbosslogexample/LoggerServlet.java
+++ src/main/java/org/emamotor/jboss/as7/jbosslogexample/LoggerServlet.java
@@ -28,8 +28,7 @@ public class LoggerServlet extends HttpServlet {
     }
 
     /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
@@ -44,8 +43,7 @@ public class LoggerServlet extends HttpServlet {
     }
 
     /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
